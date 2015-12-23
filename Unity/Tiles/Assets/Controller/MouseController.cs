9a9a3d968415adc27f8ab6b614a5a9bf0ea53906
Renaat Meeuws <dev@rmeeuws.be>
2015-12-23 14:01:30 +0100

    hammer icon for building room

---
+++ Unity/Tiles/Assets/Controller/MouseController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using Submarine.Model;
+
+namespace Submarine.Controller {
+	public class MouseController : MonoBehaviour {
+
+		public GameObject cursorBuilder;
+
+		// Use this for initialization
+		void Start () {
+	
+		}
+	
+		// Update is called once per frame
+		void Update () {
+			Vector3 currentMousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			currentMousePosition.z = 0; // set Z to zero so mouse position isn't on the camera (and be clipped so it isn't visible)
+
+			WorldController world = GameObject.FindObjectOfType<WorldController>;
+
+			Vector3 spaceBelowMouseCoordinates = world.GetTitleAtWorldCoordinates (currentMousePosition);
+					
+			cursorBuilder.transform.position = spaceBelowMouseCoordinates; 
+		}
+	}
+
+}
