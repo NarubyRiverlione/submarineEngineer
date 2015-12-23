ca859b343f7a620a7e1a6fb5904be266b8a536f4
Renaat Meeuws <dev@rmeeuws.be>
2015-12-23 14:08:07 +0100

    added mouse controller : * should Builder icon when over Tile take can contain a room * left click = build Bridge room

--- Unity/Tiles/Assets/Controller/MouseController.cs
+++ Unity/Tiles/Assets/Controller/MouseController.cs
@@ -17,11 +17,29 @@ namespace Submarine.Controller {
 			Vector3 currentMousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 			currentMousePosition.z = 0; // set Z to zero so mouse position isn't on the camera (and be clipped so it isn't visible)
 
-			WorldController world = GameObject.FindObjectOfType<WorldController>;
+			// move builder icon to tile below mouse
+			WorldController world = WorldController.instance; //GameObject.FindObjectOfType<WorldController> ();
+			Tile tileBelowMouse = world.GetTileAtWorldCoordinates (currentMousePosition);
+			// reset title if title isn't buildable 
+			if (tileBelowMouse != null && !tileBelowMouse.canContainRoom)
+				tileBelowMouse = null;
+				
+			if (tileBelowMouse != null) { // only show builder icon if mouse is above a tile
+				Vector3 spaceBelowMouseCoordinates = new Vector3 (tileBelowMouse.X, tileBelowMouse.Y, 0);
+				cursorBuilder.transform.position = spaceBelowMouseCoordinates;
+				cursorBuilder.SetActive (true);
+			}
+			else {
+				cursorBuilder.SetActive (false); // hide if cursor isn"t on a tile
+			}	
 
-			Vector3 spaceBelowMouseCoordinates = world.GetTitleAtWorldCoordinates (currentMousePosition);
-					
-			cursorBuilder.transform.position = spaceBelowMouseCoordinates; 
+			// change title type if clicked on (release left mouse)
+			if (Input.GetMouseButtonUp (0)) {
+				if (tileBelowMouse != null) {//check were above a tile
+					world.mySub.AddTileToRoom (tileBelowMouse.X, tileBelowMouse.Y, RoomType.Bridge);
+				
+				}
+			}
 		}
 	}
 
