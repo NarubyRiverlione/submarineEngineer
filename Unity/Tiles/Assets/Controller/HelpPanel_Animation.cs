6e3813c5e4e46c733e2c60ab33f17a303c13339f
Renaat Meeuws <developer@rmeeuws.be>
2016-01-01 18:33:43 +0100

    UI Panel smaller for 13" laptop UI panel help : hide + show animation

---
+++ Unity/Tiles/Assets/Controller/HelpPanel_Animation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelpPanel_Animation : MonoBehaviour {
+
+	bool ShowingHelp = false;
+
+	public GameObject RemoveThisPanel;
+
+	public void ToggleShowHelp (Animator animator) {
+		ShowingHelp = !ShowingHelp;
+		animator.SetBool ("ShowHelp", ShowingHelp);
+		RemoveThisPanel.SetActive (!ShowingHelp);
+	
+	}
+}
