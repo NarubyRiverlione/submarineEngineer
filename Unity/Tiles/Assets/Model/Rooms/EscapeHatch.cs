061ee9cc9d2beb014ad5049b697520b439d15cff
Renaat Meeuws <renaat@rmeeuws.be>
2016-01-03 16:23:15 +0100

    Escape hatch must be connected to top of submarine

--- Unity/Tiles/Assets/Model/Rooms/EscapeHatch.cs
+++ Unity/Tiles/Assets/Model/Rooms/EscapeHatch.cs
@@ -11,13 +11,26 @@ namespace Submarine.Model {
 		}
 
 		public override bool IsLayoutValid {
-			get {
-				return Size >= MinimimValidSize ? true : false;
+			get { // check size req.
+				bool sizeOk = Size >= MinimimValidSize;
+				// should be connected  to the Bride tower
+				bool locationValid = false;
+
+				int belowTopOfSub = inSub.heightOfSub - inSub.heightOfBridgeTower - 1;
+
+				foreach (Point coord in coordinatesOfTilesInRoom) {
+					Tile checkTile = inSub.GetTileAt (coord.x, coord.y);
+					if (checkTile.Y == belowTopOfSub)
+						locationValid = true;
+				}
+
+				return sizeOk && locationValid; // all req needs true
 			}
 		}
 
 		public EscapeHatch (RoomType ofThisRoomType, Sub sub, int minSize, int capPerTile, List<Resource> reqRes) : base (ofThisRoomType, sub, minSize, capPerTile, reqRes) {
-			IsAccessable = false;
+			//SetRoomValidationText ();
+			ValidationText += " and must be connected to the top of the submarine.";
 		}
 	}
 }
